4a251b3b62e542effb2a4697f114a2fa6b1a2fdd
Armel Gouriou <armel.gouriou@gmail.com>
2017-09-12 19:49:18 +0200

    feat: script for heroku to start index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express  = require('express');
 const app      = express();                               
 
-app.use(express.static(__dirname + '/dist'));                 
+app.use(express.static(__dirname + '/public'));                 
 
 app.listen(process.env.PORT || 3000);
 console.log("App listening on port " + (process.env.PORT || 3000));
