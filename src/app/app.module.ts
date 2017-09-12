86d459250d952996540d4dd8152e4f513e60b4de
Armel Gouriou <gouriou@SQI38.LOCAL>
2017-09-12 14:56:56 +0200

    feat: add sentry & error component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import * as Raven from 'raven-js';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {ErrorHandler, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './error/error.component';
+
+Raven
+    .config('https://12f96595f52d46b1beaaf085d3c5abe6@sentry.io/215970')
+    .install();
+
+export class RavenErrorHandler implements ErrorHandler {
+    handleError(err: any): void {
+        Raven.captureException(err);
+    }
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  // providers: [
+  //     {provide: ErrorHandler, useClass: RavenErrorHandler}
+  // ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
